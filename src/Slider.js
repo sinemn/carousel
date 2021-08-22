abda6a91d48d101b63b9887a7c6d45fe1f2360b6
Sinem Nurioglu <sinemnurioglu@hotmail.com>
2021-08-22 17:14:10 -0400

    image path fixed,formatted

--- src/Slider.js
+++ src/Slider.js
@@ -1,20 +1,21 @@
 import { React, useState } from "react";
 import "./carousel.scss";
-import classes from './style/Images.module.css'
-import austria from './img/austria.jpeg'
-import colombia from './img/colombia.jpeg'
-import italy from './img/italy.jpeg'
-import mountain from './img/mountain.jpeg'
-import sunrise from './img/sunrise.jpeg'
+import { ArrowBack, ArrowForward } from "@material-ui/icons";
+
+import classes from "./style/Images.module.css";
+import austria from "./img/austria.jpeg";
+import colombia from "./img/colombia.jpeg";
+import italy from "./img/italy.jpeg";
+import mountain from "./img/mountain.jpeg";
+import sunrise from "./img/sunrise.jpeg";
 
 function Carousel() {
-  
   const DATA = [
-    { id:1,src: austria, title: "austria" },
-    { id:2,src: colombia, title: "colombia" },
-    { id:3,src:italy, title: "italy" },
-    { id:4,src:mountain, title: "mountain" },
-    { id:5,src: sunrise, title: "sunrise" },
+    { id: 1, src: austria, title: "austria" },
+    { id: 2, src: colombia, title: "colombia" },
+    { id: 3, src: italy, title: "italy" },
+    { id: 4, src: mountain, title: "mountain" },
+    { id: 5, src: sunrise, title: "sunrise" },
   ];
   let carArr = DATA.length;
   const [x, setX] = useState(0);
@@ -23,33 +24,28 @@ function Carousel() {
   };
 
   const goRight = () => {
-    x === -100 * (carArr- 1) ? setX(0) : setX(x - 100);
+    x === -100 * (carArr - 1) ? setX(0) : setX(x - 100);
   };
   return (
     <div className="carousel">
       {DATA.map((item, index) => {
-          
         return (
-            
           <div
             key={index}
             className="carousel__item"
             style={{ transform: `translateX(${x}%)` }}
           >
-            <img src={item.src} className={classes.image} alt={item.title}/>
+            <img src={item.src} className={classes.image} alt={item.title} />
           </div>
         );
       })}
       <button className="carousel__buttons" id="left" onClick={goLeft}>
-        left
+        <ArrowBack />
       </button>
       <button className="carousel__buttons" id="right" onClick={goRight}>
-        right
+        <ArrowForward />
       </button>
-      <h2>Deneeme</h2>
-     
     </div>
-    
   );
 }
 
